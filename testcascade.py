d8146631f908b45a182bdd571423d2b38cdd843b
Joakim Soderberg <joakim.soderberg@gmail.com>
2014-05-26 11:02:36 +0200

    Calculate average match size + show option.

--- testcascade.py
+++ testcascade.py
@@ -7,6 +7,7 @@ import cv2
 import cv2.cv as cv
 from common import clock, draw_str
 import argparse
+import glob
 
 def detect(img, cascade, minsize):
     rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3, minSize=minsize, flags = cv.CV_HAAR_SCALE_IMAGE)
@@ -26,6 +27,9 @@ if __name__ == '__main__':
     parser.add_argument("--cascade", metavar = "CASCADE", 
                     help = "The path to the cascade xml file.")
 
+    parser.add_argument("--show", action = "store_true",
+                    help = "Show the images while iterating over them.")
+
     parser.add_argument("--pause_fail", action = "store_true",
                     help = "Pause on failed match.")
 
@@ -42,17 +46,27 @@ if __name__ == '__main__':
                     help = "Delay this many seconds between images.", default = 0.0)
 
     parser.add_argument("images", metavar = "IMAGE", nargs = "+",
-                    help = "The Catcierge match images to test.")
+                    help = "The Catcierge match images to test. If a directory is specied, all .png files in that directory are used.")
 
     args = parser.parse_args()
 
     cascade = cv2.CascadeClassifier(args.cascade)
     #nested = cv2.CascadeClassifier(nested_fn)
 
-    img_count = 0
-    match_count = 0
+    image_paths = []
 
     for img_path in args.images:
+        if os.path.isdir(img_path):
+            image_paths += glob.glob(img_path + "/*.png")
+        else:
+            image_paths.append(img_path)
+
+    img_count = 0
+    match_count = 0
+    w_sum = 0
+    h_sum = 0
+
+    for img_path in image_paths:
         print("%s" % img_path)
         img = cv2.imread(img_path)
         if img == None:
@@ -63,6 +77,7 @@ if __name__ == '__main__':
 
         t = clock()
         rects = detect(gray, cascade, (args.min_width, args.min_height))
+        match_ok = (len(rects) > 0)
         vis = img.copy()
         draw_rects(vis, rects, (0, 255, 0))
         print("Found %d matches" % len(rects))
@@ -70,17 +85,26 @@ if __name__ == '__main__':
         for x1, y1, x2, y2 in rects:
             roi = gray[y1:y2, x1:x2]
             vis_roi = vis[y1:y2, x1:x2]
-            draw_str(vis, (20, 40), "w: %d h: %d" % ((x2 - x1), (y2 - y1)))
+            w = (x2 - x1)
+            h = (y2 - y1)
+            w_sum += w
+            h_sum += h
+            draw_str(vis, (20, 40), "w: %d h: %d" % (w, h))
         dt = clock() - t
 
         draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
-        cv2.imshow('catcierge', vis)
+
+        show_on_ok = (match_ok and args.pause_ok)
+        show_on_fail = (not match_ok and args.pause_fail)
+
+        if args.show or show_on_ok or show_on_fail:
+            cv2.imshow('catcierge', vis)
 
         img_count += 1
 
         time.sleep(args.frame_delay)
 
-        if (len(rects) > 0):
+        if match_ok:
             match_count += 1
 
             if args.pause_ok:
@@ -91,7 +115,14 @@ if __name__ == '__main__':
         if 0xFF & cv2.waitKey(5) == 27:
             break
 
-    print("%d of %d matches ok (%d)" % (match_count, img_count, float(match_count) / img_count))
+    if img_count > 0:
+        print("%d of %d matches ok (%d)" % (match_count, img_count, float(match_count) / img_count))
+
+        w_avg = float(w_sum) / img_count
+        h_avg = float(h_sum) / img_count
+        print("(%f, %f) average size of match" % (w_avg, h_avg))
+    else:
+        print "No images specified ..."
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
